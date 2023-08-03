b73db5c427584462b523bdd8197f5c0bfee3bb83
pilagod <pilagooood@gmail.com>
2023-08-03 15:02:53 +0800

    add comments

--- src/interfaces/IPlugin.sol
+++ src/interfaces/IPlugin.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// IPlugin is just served as an example, not the final specification.
 interface IPlugin {
     struct PluginPermission {
         address target;
--- src/interfaces/IValidator.sol
+++ src/interfaces/IValidator.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import { UserOperation } from "@aa/interfaces/UserOperation.sol";
 
+// IValidator is just served as an example, not the final specification.
 interface IValidator {
     function validateUserOp(
         UserOperation calldata userOp,
